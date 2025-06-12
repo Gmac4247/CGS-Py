3cb04896e77c4b4e305043c697f0bc0e98e14b60
Gaál Sándor <gmac4247@gmail.com>
2025-06-12 16:17:01 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="basic-geometry",
+    version="1.0.0",
+    description="Exact geometric formulas for area, circumference, and volume",
+    author="Gaál Sándor",
+    author_email="gmac4247@yahoo.com",
+    py_modules=["basic-geometry"],
+    python_requires=">=3.7",
+    license="MIT",
+)
