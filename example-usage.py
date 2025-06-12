3db2f765b51fd94e0aa830daaf6f35a2e7bf935d
Gaál Sándor <gmac4247@gmail.com>
2025-06-12 16:01:35 +0200

    Create example-usage.py

---
+++ example-usage.py
@@ -0,0 +1,9 @@
+import geometry
+
+r = 2
+h = 5
+
+print(geometry.area_of_circle(r))            # 12.8
+print(geometry.circumference_of_circle(r))   # 12.8
+print(geometry.volume_of_sphere(r))          # ≈28.84
+print(geometry.volume_of_cone(r, h))         # ≈11.31
