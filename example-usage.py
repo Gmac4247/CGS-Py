717278b9173f6decaa9b7e6c94610a33239a90ef
Gaál Sándor <gmac4247@gmail.com>
2025-06-13 13:16:47 +0200

    Update example-usage.py
    Added the term "exact_" to the keys to avoid possible conflict.

--- example-usage.py
+++ example-usage.py
@@ -3,7 +3,7 @@ import geometry
 r = 2
 h = 5
 
-print(geometry.area_of_circle(r))            # 12.8
-print(geometry.circumference_of_circle(r))   # 12.8
-print(geometry.volume_of_sphere(r))          # ≈28.84
-print(geometry.volume_of_cone(r, h))         # ≈11.31
+print(geometry.exact_area_of_circle(r))            # 12.8
+print(geometry.exact_circumference_of_circle(r))   # 12.8
+print(geometry.exact_volume_of_sphere(r))          # ≈28.84
+print(geometry.exact_volume_of_cone(r, h))         # ≈11.31
