73c81b2eb969fdb0ed483fce3d696e48d51f59a1
Gaál Sándor <gmac4247@gmail.com>
2025-06-15 18:56:02 +0200

    Update example_usage.py

--- example_usage.py
+++ example_usage.py
@@ -5,7 +5,7 @@ import basic_geometry
 """ Usage example of functions """
 """ Replacy legacy expressions by the exact ones where the output is. """
 
-print(basic_geometry.exact_area_of_circle(r))   
-print(basic_geometry.exact_circumference_of_circle(r)) 
-print(basic_geometry.exact_volume_of_sphere(r))   
-print(basic_geometry.exact_volume_of_cone(r, h))  
+print(basic_geometry.exact_circle_area(r))   
+print(basic_geometry.exact_circumference(r)) 
+print(basic_geometry.exact_sphere_volume(r))   
+print(basic_geometry.exact_cone_volume(r, h))  
