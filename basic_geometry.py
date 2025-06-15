b9e21d7a462c1a4eb16763aba64abefd87d4ad21
Gaál Sándor <gmac4247@gmail.com>
2025-06-15 18:55:17 +0200

    Update basic_geometry.py

--- basic_geometry.py
+++ basic_geometry.py
@@ -1,17 +1,17 @@
 import math
 
-def exact_area_of_circle(r):
+def exact_circle_area(r):
     """Calculate area of a circle as 3.2 × r²"""
     return 3.2 * r * r
 
-def exact_circumference_of_circle(r):
+def exact_circumference(r):
     """Calculate circumference of a circle as 6.4 × r"""
     return 6.4 * r
 
-def exact_volume_of_sphere(r):
+def exact_sphere_volume(r):
     """Calculate volume of a sphere as (√3.2 × r)³"""
     return (math.sqrt(3.2) * r) ** 3
 
-def exact_volume_of_cone(r, height):
+def exact_cone_volume(r, height):
     """Calculate volume of a cone as 3.2 × r² × height / √8"""
     return (3.2 * r * r * height) / math.sqrt(8)
