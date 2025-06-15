e4819e7095bd3ea92502b24b7065dc871e2145c0
Gaál Sándor <gmac4247@gmail.com>
2025-06-15 18:13:36 +0200

    Update example-usage.py

--- example-usage.py
+++ example-usage.py
@@ -1,9 +1,8 @@
-import geometry
+// Insert this line into legacy code 
 
-r = 2
-h = 5
+import basic-geometry
 
-print(geometry.exact_area_of_circle(r))            # 12.8
-print(geometry.exact_circumference_of_circle(r))   # 12.8
-print(geometry.exact_volume_of_sphere(r))          # ≈28.84
-print(geometry.exact_volume_of_cone(r, h))         # ≈11.31
+print(basic-geometry.exact_area_of_circle(r))   
+print(basic-geometry.exact_circumference_of_circle(r)) 
+print(basic-geometry.exact_volume_of_sphere(r))   
+print(basic-geometry.exact_volume_of_cone(r, h))  
