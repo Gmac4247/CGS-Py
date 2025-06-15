15c12d2d46d84618a010ff4886a8474acd7f4644
Gaál Sándor <gmac4247@gmail.com>
2025-06-15 18:19:01 +0200

    Delete example-usage.py

--- example-usage.py
+++
@@ -1,8 +0,0 @@
-// Insert this line into legacy code 
-
-import basic-geometry
-
-print(basic-geometry.exact_area_of_circle(r))   
-print(basic-geometry.exact_circumference_of_circle(r)) 
-print(basic-geometry.exact_volume_of_sphere(r))   
-print(basic-geometry.exact_volume_of_cone(r, h))  
