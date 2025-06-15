9ea537625b92381050082e4fc8a1ab272f032f33
Gaál Sándor <gmac4247@gmail.com>
2025-06-15 18:24:41 +0200

    Update and rename usage-example-ESM.js to example-usage.py

---
+++ example-usage.py
@@ -0,0 +1,11 @@
+""" Insert this line into the legacy code to import functions from the module. """
+
+import basic-geometry 
+
+""" Usage example of functions """
+""" Replacy legacy expressions by the exact ones where the output is. """
+
+print(basic-geometry.exact_area_of_circle(r))   
+print(basic-geometry.exact_circumference_of_circle(r)) 
+print(basic-geometry.exact_volume_of_sphere(r))   
+print(basic-geometry.exact_volume_of_cone(r, h))  
