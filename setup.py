4706453bba085fcafbdacd46c51cda59b0668d8a
Gaál Sándor <gmac4247@gmail.com>
2025-06-15 18:33:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name="Core Geometric System ™",
-    version="1.0.1",
+    version="1.0.3",
     description="Exact geometric formulas for area, circumference, and volume",
     author="Gaál Sándor",
     author_email="gmac4247@yahoo.com",
-    py_modules=["basic-geometry"],
+    py_modules=["basic_geometry"],
     python_requires=">=3.7",
     license="MIT",
 )
