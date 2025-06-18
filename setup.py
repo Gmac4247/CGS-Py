a701b689187cbb14e9c2f2237c7a2e3023585006
Gaál Sándor <gmac4247@gmail.com>
2025-06-18 20:46:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="Core Geometric System ™",
-    version="1.0.5",
-    description="Exact geometric formulas for calculating circumference, area, and volume, and aligned trigonometry",
+    version="1.0.6",
+    description="Geometric calculator patch with exact formulas for circumference, area of circles, and volume of spheres and cones",
     author="Gaal Sandor",
     author_email="gmac4247@yahoo.com",
     py_modules=["core_geometric_system"],
